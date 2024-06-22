b652b6f2f252e1870bad60b1230b9d3e61e212b3
irzal <121763889+mathjsx@users.noreply.github.com>
2024-06-22 17:57:16 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 // pages/index.tsx
 import Head from 'next/head'
-import Card from '../components/card.tsx'
+import Card from '../components/Card'
 
 const Home: React.FC = () => {
   const colors = ['bg-red-200', 'bg-green-200', 'bg-blue-200', 'bg-yellow-200', 'bg-purple-200'];
